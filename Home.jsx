2710b3c80c1747849d5e9dbe72f1f1a9456417ae
Taruntaruns <127745170+Taruntaruns@users.noreply.github.com>
2023-12-12 08:07:16 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet cum
+        voluptatibus omnis optio ipsum labore ipsam eos aliquam excepturi
+        dolore.
+      </p>
+    </div>
+  );
+};
